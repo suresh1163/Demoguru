76be4b05c28dbf8e6aa10a5fb95432efc66500a1
suresh1163 <suresh.1163@gmail.com>
2019-10-11 23:46:34 +0530

    new code

--- src/test/java/com/stepdefinition/Booking.java
+++ src/test/java/com/stepdefinition/Booking.java
@@ -17,11 +17,13 @@ public class Booking extends Base{
 	 System.out.println("booking page");
 	 launch("http://demo.guru99.com/telecom/addcustomer.php");
 	}
+	
 
 	@When("enter booking detials")
 	public void enter_booking_detials(DataTable dataTable) throws InterruptedException {
-	    List<String> li=dataTable.asList(String.class);
-	     type(b.getFname(), li.get(0));
+		List<String> li=dataTable.asList(String.class);
+		click(b.getCheck());
+	    type(b.getFname(), li.get(0));
 	    type(b.getLname(),li.get(1));
 	    type(b.getEmail(),li.get(2));
 	    type(b.getAddress(),li.get(3));
@@ -29,7 +31,18 @@ public class Booking extends Base{
 	    Thread.sleep(5000);
 	    click(b.getSubmit());
 	    	}
-
+	@When("enter details{string},{string},{string},{string},{string}")
+	public void enter_details(String s1, String s2, String s3, String s4, String s5) throws InterruptedException {
+		click(b.getCheck());
+		type(b.getFname(),s1);
+		type(b.getLname(),s2);
+		type(b.getEmail(),s3);
+		type(b.getAddress(),s4);
+		type(b.getMobno(),s5);
+		Thread.sleep(20000);
+		click(b.getSubmit());
+	}
+	
 	@Then("customer see ID")
 	public void customer_see_ID() {
 	  Hook.driver.getCurrentUrl();
--- src/test/resources/Booking.feature
+++ src/test/resources/Booking.feature
@@ -8,3 +8,15 @@ Feature: Title of your feature
     When enter booking detials
     |suresh|D|suresh@gmail.com|chennai|8877546809|
     Then customer see ID
+    
+@tag2
+  Scenario Outline: Title of your scenario outline
+    Given customer on booking page
+    When enter details"<firstname>","<lastname>","<email>","<address>","<mobile>"
+    Then customer see ID
+  
+    Examples: 
+      |firstname | lastname | email            |address       |mobile    |
+      | udhay    | M        | udhay@gmail.com  |thiruvanamalai|9876543768|
+      | dinesh   | kumar    | dinesh@gmail.com |pallavaram    | 764808648|
+    
