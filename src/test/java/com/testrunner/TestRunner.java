cc02ef2094dc4d7414c4109d91f75f26957b7662
suresh1163 <suresh.1163@gmail.com>
2019-10-16 23:43:33 +0530

    jsonreport

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\bookwithexcel.feature",glue="com.stepdefinition",dryRun=false, monochrome=true, plugin="html:target")
+@CucumberOptions(features="src\\test\\resources\\bookwithexcel.feature",glue="com.stepdefinition",dryRun=false, monochrome=true, plugin= {"html:target","json:target/cucur.json"})
 public class TestRunner {	
 }
